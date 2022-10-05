33bde7626dcaa01f688da0f1ae0f37f8bdb622a7
codu2 <khub0929@gmail.com>
2022-10-05 21:23:24 +0900

    Categories, CategoryCard, FeaturedRow, RestaurantCard component 및 homescreen header, search input, contents ui 생성

---
+++ screens/HomeScreen.js
@@ -0,0 +1,76 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import {
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  UserIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView className="bg-white pt-5">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location <ChevronDownIcon size={20} color="#00ccbb" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00ccbb" />
+      </View>
+
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00ccbb" />
+      </View>
+
+      <ScrollView
+        className="bg-gray-100 flex-1"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        <Categories />
+        <FeaturedRow
+          id="123"
+          title="Featured"
+          description="Paid placements from our partners"
+        />
+        <FeaturedRow
+          id="456"
+          title="Tasty Discounts"
+          description="Everyone's been enjoying these juicy discounts!"
+        />
+        <FeaturedRow
+          id="789"
+          title="Offers near you!"
+          description="Why not support your local restaurant tonight!"
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
