87c6ca4b208b7c6de45124e232b7cf1f23c88ee1
codu2 <khub0929@gmail.com>
2022-10-07 10:44:56 +0900

    redux provider 설정 및 basketscreen, preparingscreen, deliveryscreen 추가, react-native-animatable을 이용한 preparingscreen 애니메이션, react-native-progress를 이용한 pregress circle과 bar, react-native-maps를 이용해 restaurant 위치를 표시하는 맵 기능 추가

--- App.js
+++ App.js
@@ -3,30 +3,61 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import BasketScreen from "./screens/BasketScreen";
+import PreparingOrderScreen from "./screens/PreparingOrderScreen";
+import DeliveryScreen from "./screens/DeliveryScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="Restaurant"
-            component={RestaurantScreen}
-            options={{
-              headerShown: false,
-            }}
-          />
-        </Stack.Navigator>
-      </TailwindProvider>
+      <Provider store={store}>
+        <TailwindProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Restaurant"
+              component={RestaurantScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Basket"
+              component={BasketScreen}
+              options={{
+                headerShown: false,
+                presentation: "modal",
+              }}
+            />
+            <Stack.Screen
+              name="PreparingOrder"
+              component={PreparingOrderScreen}
+              options={{
+                headerShown: false,
+                presentation: "fullScreenModal",
+              }}
+            />
+            <Stack.Screen
+              name="Delivery"
+              component={DeliveryScreen}
+              options={{
+                headerShown: false,
+                presentation: "fullScreenModal",
+              }}
+            />
+          </Stack.Navigator>
+        </TailwindProvider>
+      </Provider>
     </NavigationContainer>
   );
 }
